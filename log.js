434ca383f36766df82605156f74aad4584ef2919
liangjun.zhong <zhongliangjun1@gmail.com>
2014-06-03 18:16:10 +0800

    adjust log

--- log.js
+++ log.js
@@ -1,6 +1,21 @@
 
 var bunyan = require('bunyan');
 
+var fs = require('fs');
+
+if (!fs.existsSync('/data')) {
+	fs.mkdirSync('/data');
+};
+if (!fs.existsSync('/data/applogs')) {
+	fs.mkdirSync('/data/applogs');
+};
+if (!fs.existsSync('/data/applogs/dp-qzone-feed-count')) {
+	fs.mkdirSync('/data/applogs/dp-qzone-feed-count');
+};
+if (!fs.existsSync('/data/applogs/dp-qzone-feed-count/logs')) {
+	fs.mkdirSync('/data/applogs/dp-qzone-feed-count/logs');
+};
+
 var log = bunyan.createLogger({
   name: 'dp-qzone-feed-count',
   streams: [
@@ -11,8 +26,7 @@ var log = bunyan.createLogger({
     {
       level: 'error',
       type: 'rotating-file',
-      //path: '/data/applogs/dp-qzone-feed-count/logs/app.log',  // log ERROR and above to a file
-      path: './logs/app.log',
+      path: '/data/applogs/dp-qzone-feed-count/logs/app.log',  // log ERROR and above to a file
       period: '1d',   // daily rotation
       count: 14        // keep 14 back copies
     }
